bb228eee9a5a9d68fbc42df3e7176398f1f9bf76
ruanshiron <sandt26@gmail.com>
2020-11-09 08:43:20 +0700

    Navbar fixed

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,29 @@
 import Link from 'next/link'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
-
-  const isActive = (route) => {
-    return (route == router.pathname)
-  }
 
   return (
     <Navbar collapseOnSelect expand="md" bg="light" variant="light">
       <Container>
         <Link href="/">
-          <Navbar.Brand>Food Guide</Navbar.Brand>
+          <Navbar.Brand className="mr-4">
+            <FontAwesomeIcon icon="drumstick-bite" className="mr-2" />
+            Food Guide
+          </Navbar.Brand>
         </Link>
+        <Form className="m-auto" inline>
+          <FormControl size="sm" type="text" placeholder="料理名・食材でレシピをさがす" className="mr-sm-2 flex-1" />
+        </Form>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
             <Link href="/recipes">
-              <Nav.Link>Recipes</Nav.Link>
+              <Nav.Link active as="a" href="/recipes">レシピカテゴリ</Nav.Link>
+            </Link>
+            <Link href="/recipes">
+              <Nav.Link active as="a" href="/recipes">ホット</Nav.Link>
             </Link>
           </Nav>
         </Navbar.Collapse>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
 
 function App({ Component, pageProps }) {
   return (
