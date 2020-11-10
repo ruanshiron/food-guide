abc4d37779e8c23212c5f7110bb7107f071d7d83
ruanshiron <sandt26@gmail.com>
2020-11-10 10:58:05 +0700

    This commit to proof that i'm not lazy

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,9 +1,71 @@
-import Head from 'next/head'
+import { Typography, List, Space } from "antd";
+import React from "react"
+import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+import Head from "next/head";
+
+const listData = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: "https://ant.design",
+    title: `ant design part ${i}`,
+    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+    description:
+      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+    content:
+      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+  });
+}
+
+const IconText = ({ icon, text }) => (
+  <Space>
+    {React.createElement(icon)}
+    {text}
+  </Space>
+);
 
 export default function Home() {
   return (
-    <div>
-      recipes
-    </div>
-  )
+    <>
+      <Typography>Recipes</Typography>
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={listData}
+        renderItem={(item) => (
+          <List.Item
+            key={item.title}
+            actions={[
+              <IconText
+                icon={StarOutlined}
+                text="156"
+                key="list-vertical-star-o"
+              />,
+              <IconText
+                icon={LikeOutlined}
+                text="156"
+                key="list-vertical-like-o"
+              />,
+              <IconText
+                icon={MessageOutlined}
+                text="2"
+                key="list-vertical-message"
+              />,
+            ]}
+            extra={
+              <img
+                height={128}
+                alt="logo"
+                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            }
+          >
+            <List.Item.Meta
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.description}
+            />
+          </List.Item>
+        )}
+      />
+    </>
+  );
 }
