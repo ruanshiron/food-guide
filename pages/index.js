206726945159372b406ee13a2c25cff9b6f7ee1b
ruanshiron <sandt26@gmail.com>
2020-11-11 21:07:19 +0700

    This commit to proof that i'm so fancy

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,133 @@
-import Head from 'next/head'
-import { Typography, Divider, Row, Col, Card } from "antd";
+import { Typography, Divider, Row, Col, Card, Tag } from "antd";
+import Link from "next/link";
+
+const data = [
+  {
+    title: "優しい味わい！大根のそぼろ煮",
+    src:
+      "https://video.kurashiru.com/production/videos/7ba2b9a0-52d0-4f88-b19d-a2e101983110/compressed_thumbnail_square_large.jpg?1472440610ng",
+  },
+  {
+    title: "大根のバタポンステーキ",
+    src:
+      "https://video.kurashiru.com/production/videos/a6fed850-d897-4ced-9855-55168ee50bb8/compressed_thumbnail_square_large.jpg?1475054146",
+  },
+  {
+    title: "千切りキャベツ山盛り！キャベツお好み焼き",
+    src:
+      "https://video.kurashiru.com/production/videos/32e91e87-7b9a-414f-b336-2f95e2e93533/compressed_thumbnail_square_large.jpg?1599532153",
+  },
+  {
+    title: "優しい味わい！大根のそぼろ煮",
+    src:
+      "https://video.kurashiru.com/production/videos/7ba2b9a0-52d0-4f88-b19d-a2e101983110/compressed_thumbnail_square_large.jpg?1472440610ng",
+  },
+  {
+    title: "優しい味わい！大根のそぼろ煮",
+    src:
+      "https://video.kurashiru.com/production/videos/7ba2b9a0-52d0-4f88-b19d-a2e101983110/compressed_thumbnail_square_large.jpg?1472440610ng",
+  },
+  {
+    title: "優しい味わい！大根のそぼろ煮",
+    src:
+      "https://video.kurashiru.com/production/videos/7ba2b9a0-52d0-4f88-b19d-a2e101983110/compressed_thumbnail_square_large.jpg?1472440610ng",
+  },
+];
+
+const chefs = [
+  { src: "https://i.imgur.com/pYpqi1v.png" },
+  {
+    src:
+      "https://scontent.fhan2-6.fna.fbcdn.net/v/t1.0-9/10353110_1568599166687430_1416722809578967040_n.jpg?_nc_cat=104&ccb=2&_nc_sid=ba80b0&_nc_ohc=0LX-Du5kFjQAX_fWGIq&_nc_ht=scontent.fhan2-6.fna&oh=766716133d6cb5fe9bfe5ffe2970e2bd&oe=5FD277F0",
+  },
+  {
+    src:
+      "https://scontent.fhan2-3.fna.fbcdn.net/v/t1.0-9/97116271_1787037028104496_322437548691423232_o.jpg?_nc_cat=108&ccb=2&_nc_sid=8bfeb9&_nc_ohc=a-GvxKi6T4MAX8-N7gr&_nc_ht=scontent.fhan2-3.fna&oh=d627088855bf587a7fb70f40a8f9348a&oe=5FD0CA5F",
+  },
+  {
+    src:
+      "https://scontent.fhan2-5.fna.fbcdn.net/v/t1.0-9/45804440_941790919348225_3914881021224419328_o.jpg?_nc_cat=109&ccb=2&_nc_sid=174925&_nc_ohc=nTTFee4BrkYAX_MVdeb&_nc_ht=scontent.fhan2-5.fna&oh=2486388042e826f952b2211fd40f327c&oe=5FD19858",
+  },
+  {
+    src:
+      "https://scontent.fhan2-6.fna.fbcdn.net/v/t1.0-9/16649203_586428338234050_6126128784764679403_n.jpg?_nc_cat=103&ccb=2&_nc_sid=cdbe9c&_nc_ohc=f_xWRr_tpesAX8zFCCy&_nc_ht=scontent.fhan2-6.fna&oh=5c80e92afaa5de0ebb7d2392ad1d76fa&oe=5FD32B2E",
+  },
+  {
+    src:
+      "https://scontent.fhan2-2.fna.fbcdn.net/v/t1.0-9/23622220_2056772447889079_8060687045552129362_n.jpg?_nc_cat=111&ccb=2&_nc_sid=0debeb&_nc_ohc=23U1R58M5rUAX_BXZKH&_nc_ht=scontent.fhan2-2.fna&oh=7b159929b03e642ca49ced1784e463be&oe=5FD05843",
+  },
+  {
+    src:
+      "https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/124022964_2713691562222505_5724321518775122311_o.jpg?_nc_cat=105&ccb=2&_nc_sid=19026a&_nc_ohc=KFfdlmhNtQ0AX9uNSuf&_nc_ht=scontent.fhan2-4.fna&oh=2157e76fda4d3d82f5c69d37cec897b9&oe=5FD27CD9",
+  },
+];
 
 export default function Home() {
   return (
-    <>
-      <Typography className='home-container'>
-        <div style={{ textAlign: 'center', minHeight: 300, background: "#fdf2ca repeat 0 0 url(/images/homepage.jpeg)" }}>
-          <h1 style={{ fontSize: 60, paddingTop: 50}}>Food Guide</h1>
-          <h3>献立、簡単レシピに動画も！</h3>
-          <h3>必ず見つかるレシピサイト</h3>
-        </div>
-      </Typography>
+    <div className="container container-lg">
+      <Row>
+        <Col style={{ padding: 15 }} xs={24} md={18}>
+          <div
+            style={{
+              textAlign: "center",
+              minHeight: 300,
+              background: "#fdf2ca repeat 0 0 url(/images/homepage.jpeg)",
+              marginBottom: 30,
+            }}
+          >
+            <h1 style={{ fontSize: 60, paddingTop: 50 }}>Food Guide</h1>
+            <h3>献立、簡単レシピに動画も！</h3>
+            <h3>必ず見つかるレシピサイト</h3>
+          </div>
+          <Divider orientation="left">
+            <h2>今日のオススメ</h2>
+          </Divider>
+          <Row gutter={16}>
+            {data.map((item, index) => (
+              <Col span={8}>
+                <Card
+                  style={{ marginBottom: 16 }}
+                  hoverable
+                  cover={<img alt="example" src={item.src} />}
+                >
+                  <Link href={`/recipes/${index}`}>
+                    <Card.Meta title={item.title} />
+                  </Link>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+        <Col style={{ padding: 16 }} xs={24} md={6}>
+          <div style={{ marginBottom: 15 }}>
+            <h2>定番のキーワード</h2>
+            <Tag style={{ marginBottom: 7 }}>キャベツ</Tag>
+            <Tag style={{ marginBottom: 7 }}>豚肉</Tag>
+            <Tag style={{ marginBottom: 7 }}>なす</Tag>
+            <Tag style={{ marginBottom: 7 }}>鶏肉</Tag>
+            <Tag style={{ marginBottom: 7 }}>キャベツ</Tag>
+            <Tag style={{ marginBottom: 7 }}>豚肉</Tag>
+            <Tag style={{ marginBottom: 7 }}>なす</Tag>
+            <Tag style={{ marginBottom: 7 }}>鶏肉</Tag>
+          </div>
 
-      <Typography className='home-container'>
-        <Divider orientation="left">
-          <h2>
-            今日のオススメ
-          </h2>
-        </Divider>
-        <Row justify="space-between" gutter={16} style={{ padding: "40px 100px" }}>
-          <Col className="gutter-row" span={6}>
-            <Card
-              hoverable
-              cover={<img alt="example" src="https://video.kurashiru.com/production/videos/7ba2b9a0-52d0-4f88-b19d-a2e101983110/compressed_thumbnail_square_large.jpg?1472440610ng" />}
-            >
-              <Card.Meta title="優しい味わい！大根のそぼろ煮" description="電子レンジで下処理をするので、時短・簡便！" />
-            </Card>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <Card
-              hoverable
-              cover={<img alt="example" src="https://video.kurashiru.com/production/videos/a6fed850-d897-4ced-9855-55168ee50bb8/compressed_thumbnail_square_large.jpg?1475054146" />}
-            >
-              <Card.Meta title="大根のバタポンステーキ" description="中途半端に余った大根の消費に美味しくお召し上がりいただける一品です。" />
-            </Card>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <Card
-              hoverable
-              cover={<img alt="example" src="https://video.kurashiru.com/production/videos/32e91e87-7b9a-414f-b336-2f95e2e93533/compressed_thumbnail_square_large.jpg?1599532153" />}
-            >
-              <Card.Meta title="千切りキャベツ山盛り！キャベツお好み焼き" description="キャベツをたっぷり使用し、粉を少なめにしたお好み焼きのレシピです。" />
-            </Card>
-          </Col>
-        </Row>
-      </Typography>
-    </>
-  )
+          <div style={{ marginBottom: 15 }}>
+            <h2>最高のシェフ</h2>
+            <Row gutter={8}>
+              {chefs.map((chef) => (
+                <Col style={{ marginBottom: 8 }} xs={8} md={12}>
+                  <img
+                    width={"100%"}
+                    height={156}
+                    style={{ objectFit: "cover" }}
+                    src={chef.src}
+                  />
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
