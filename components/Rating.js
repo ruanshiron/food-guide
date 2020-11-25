4bcb6b60ed6bc69677b472cb909bcf7a2dc7c5bc
nguyenduong2612 <nguyenduong261298@gmail.com>
2020-11-26 00:24:45 +0700

    add translation

--- components/Rating.js
+++ components/Rating.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import useRatings from "../hooks/useRatings";
+import useTranslation from "../intl/useTranslation";
 
 const { Rate, Button } = require("antd");
 
 const Rating = ({ recipeID, defaultValue = 0 }) => {
   const [value, setValue] = useState(defaultValue);
   const { rate } = useRatings(recipeID);
+  const { t } = useTranslation();
+
   return (
     <>
       <Rate defaultValue={value} onChange={(v) => setValue(v)} />
       <Button type="primary" shape="round" onClick={() => rate(value)}>
-        送信
+        {t("送信")}
       </Button>
     </>
   );
