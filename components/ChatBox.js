06113dbee7a2cc3a009093dd2e5a654551a38934
ruanshiron <sandt26@gmail.com>
2020-12-30 22:29:20 +0700

    auth with chat

--- components/ChatBox.js
+++ components/ChatBox.js
@@ -13,6 +13,7 @@ import {
 } from "@chatscope/chat-ui-kit-react";
 import { useChat } from "../hooks/useChat";
 import { Button } from "antd";
+import { useAuth } from "../utils/auth/AuthProvider";
 
 export const ChatBox = () => {
   const {
@@ -24,63 +25,80 @@ export const ChatBox = () => {
     setCurrentOpponent,
     send,
   } = useChat();
+
+  const { user } = useAuth();
   return (
-    <div id="chat-box">
-      <Button
-        style={{ display: !showChatBox ? "inline-block" : "none", bottom: -2 }}
-        onClick={() => toggleChatBox()}
-      >
-        CHAT
-      </Button>
-      <div
-        style={{
-          display: showChatBox ? "block" : "none",
-          height: 300,
-          width: 500,
-          bottom: -2,
-        }}
-      >
-        <MainContainer>
-          <Sidebar position="left" scrollable={false}>
-            <ConversationList>
-              {conversations.map((conversation, index) => (
-                <Conversation
-                  key={index}
-                  name={conversation.name}
-                  active={conversation.id == currentOpponent?.id}
-                  onClick={() => {setCurrentOpponent(conversation)}}
-                ></Conversation>
-              ))}
-            </ConversationList>
-          </Sidebar>
-          <ChatContainer>
-            <ConversationHeader>
-              <ConversationHeader.Content userName={currentOpponent?.receiver} />
-              <ConversationHeader.Actions>
-                <ArrowButton direction="down" onClick={() => toggleChatBox()} />
-              </ConversationHeader.Actions>
-            </ConversationHeader>
-            <MessageList>
-              {messages
-                .filter((v) => v.opponent == currentOpponent?.id)
-                .map((message, index) => (
-                  <Message
-                    model={{
-                      message: message.text,
-                      sender: message.receiver,
-                      direction: message.sender === currentOpponent?.id ? "incoming" : "outgoing",
+    user && (
+      <div id="chat-box">
+        <Button
+          style={{
+            display: !showChatBox ? "inline-block" : "none",
+            bottom: -2,
+          }}
+          onClick={() => toggleChatBox()}
+        >
+          CHAT
+        </Button>
+        <div
+          style={{
+            display: showChatBox ? "block" : "none",
+            height: 300,
+            width: 500,
+            bottom: -2,
+          }}
+        >
+          <MainContainer>
+            <Sidebar position="left" scrollable={false}>
+              <ConversationList>
+                {conversations.map((conversation, index) => (
+                  <Conversation
+                    key={index}
+                    name={conversation.name}
+                    active={conversation.id == currentOpponent?.id}
+                    onClick={() => {
+                      setCurrentOpponent(conversation);
                     }}
-                  />
+                  ></Conversation>
                 ))}
-            </MessageList>
-            <MessageInput
-              attachButton={false}
-              placeholder="Aa"
-              onSend={(e) => send(e)}
-            />
-          </ChatContainer>
-        </MainContainer>
+              </ConversationList>
+            </Sidebar>
+            <ChatContainer>
+              <ConversationHeader>
+                <ConversationHeader.Content
+                  userName={currentOpponent?.receiver}
+                />
+                <ConversationHeader.Actions>
+                  <ArrowButton
+                    direction="down"
+                    onClick={() => toggleChatBox()}
+                  />
+                </ConversationHeader.Actions>
+              </ConversationHeader>
+              <MessageList>
+                {messages
+                  .filter((v) => v.opponent == currentOpponent?.id)
+                  .map((message, index) => (
+                    <Message
+                      model={{
+                        message: message.text,
+                        sender: message.receiver,
+                        direction:
+                          message.sender === currentOpponent?.id
+                            ? "incoming"
+                            : "outgoing",
+                      }}
+                    />
+                  ))}
+              </MessageList>
+              <MessageInput
+                attachButton={false}
+                placeholder="Aa"
+                onSend={(e) => send(e)}
+              />
+            </ChatContainer>
+          </MainContainer>
+        </div>
       </div>
-    </div>
+    )
   );
 };
